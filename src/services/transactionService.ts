86aa05b430cbcdc4703cb5d64bf76c95cdc11cee
Alexander Valencia <alex16jpv@gmail.com>
2024-07-27 14:19:04 -0500

    Refactor transactionService to use aggregation pipeline for getAllTrx
    The code changes in transactionService.ts modify the getAllTrx method to use the aggregation pipeline instead of the find method. This allows for performing more complex queries and includes additional lookup and projection stages to retrieve specific fields from related models. The purpose of these changes is to enhance the functionality and performance of retrieving transactions.

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,8 +1,43 @@
 import transactionModel from "../models/TransactionModel";
+import { MODEL_NAMES } from "../utils/models";
 
 class TransactionService {
   async getAllTrx() {
-    return await transactionModel.find();
+    return await transactionModel.aggregate([
+      {
+        $lookup: {
+          from: MODEL_NAMES.ACCOUNT,
+          localField: "from_account_id",
+          foreignField: "_id",
+          as: "from_account",
+        },
+      },
+      {
+        $lookup: {
+          from: MODEL_NAMES.ACCOUNT,
+          localField: "to_account_id",
+          foreignField: "_id",
+          as: "to_account",
+        },
+      },
+      {
+        $unwind: {
+          path: "$from_account",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $unwind: {
+          path: "$to_account",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          to_account_name: "$to_account.name",
+        },
+      },
+    ]);
   }
 }
 
